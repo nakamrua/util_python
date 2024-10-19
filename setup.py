7d8aa2f349056e7a66f574535c67651ab1d0eb1e
nakamura <fllovfgv@gmail.com>
2024-10-19 16:22:12 +0900

    add operations.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='util_python',
+    version='0.1.0',
+    description='utility programs for generative ai trial',
+    author='snakamura',
+    author_email='fllovfgv@gmail.com',
+    url='https://github.com/nakamrua/util_python',
+    packages=find_packages(),
+    python_requires='>=3.12',
+)
